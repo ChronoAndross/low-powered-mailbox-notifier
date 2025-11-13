4972dfff0909483d2c54c69f9b8cfcfb247891e3
ChronoAndross <andross928@gmail.com>
2025-11-12 23:20:11 -0500

    low powered implementation part 1

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from twilio.twilio import Twilio
 from sensors.sensors import Sensors
 import time
+import sys
 
-def main():
-    # instantiate sensors
-    sensors_api = Sensors(31, 33, 37)
+def _low_powered_implementation(sensors_api: Sensors, twilio_api: Twilio, phone_number_from: str, phone_number_to: str):
+    sensors_api.setup_event_callbacks(twilio_api.send_text_message(phone_number_from, phone_number_to, "this is a sample message!!!"))
+    
 
-    # keys.txt should be a text file that is created locally and contains the twilio account sid, access key, a from number, and a to number.
-    keys = open("keys.txt").readline().split(",")
-    account_sid = keys[0]
-    auth_token = keys[1]
-    phone_number_from = keys[2]
-    phone_number_to = keys[3]
-    twilio_api = Twilio(account_sid, auth_token)
+def _high_powered_implementation(sensors_api: Sensors, twilio_api: Twilio, phone_number_from: str, phone_number_to: str):
     while 1:
         # this doesn't stop for now
         print("sleep for 0.5 seconds...")
@@ -23,5 +18,25 @@ def main():
         else:
             print("didn't detect anything...")
 
+def main(low_power_execution: bool):
+    # instantiate sensors
+    sensors_api = Sensors(31, 33, 37)
+
+    # keys.txt should be a text file that is created locally and contains the twilio account sid, access key, a from number, and a to number.
+    keys = open("keys.txt").readline().split(",")
+    account_sid = keys[0]
+    auth_token = keys[1]
+    phone_number_from = keys[2]
+    phone_number_to = keys[3]
+    twilio_api = Twilio(account_sid, auth_token)
+
+    if low_power_execution:
+        _low_powered_implementation(sensors_api, twilio_api, phone_number_from, phone_number_to)
+    else:
+        _high_powered_implementation(sensors_api, twilio_api, phone_number_from, phone_number_to)
+
 if __name__ == "__main__":    
-    main()
+    low_power_execution = False
+    if len(sys.argv) > 1 and (sys.argv[0] == "low_power" or sys.argv[0] == "lp"):
+        low_power_execution = True
+    main(low_power_execution)
--- sensors/sensors.py
+++ sensors/sensors.py
@@ -13,6 +13,15 @@ class Sensors():
 		self.gpio_pir_1 = gpio_pir_1
 		self.gpio_pir_2 = gpio_pir_2
 		self.gpio_microphone = gpio_microphone
+		self.event_callbacks_setup = False
+
+	def setup_event_callbacks(self, callback: callable):
+		if self.event_callbacks_setup:
+			print("callbacks already set up, skipping")
+		else:
+			gpio.add_event_detect(self.gpio_microphone, gpio.RISING, callback, bouncetime=1000)
+			gpio.add_event_detect(self.gpio_pir_1, gpio.RISING, callback, bouncetime=1000)
+			gpio.add_event_detect(self.gpio_pir_2, gpio.RISING, callback, bouncetime=1000)
 	
 	def detect(self) -> bool:
 		if gpio.input(self.gpio_pir_1):
